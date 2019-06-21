c1ba2c8f55e408a3cfe833649d2f86bb0397af48
laragh <laragh@163.com>
2019-06-21 19:08:53 +0800

    init

--- Centos7/install.sh
+++ Centos7/install.sh
@@ -9,7 +9,7 @@ source ${CURRENT_DIR}/../common/common.sh
 MYSQL_ROOT_PASSWORD=`random_string`
 
 function init_system {
-    yum -y install wget
+    yum -y install wget curl
     
     # 英文和时区的修改
     localectl  set-locale LANG=en_US.UTF-8
@@ -27,7 +27,7 @@ function init_alias {
 
 function init_repositories {
     # 使用阿里更新源
-    cp /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
+    rm -rf /etc/yum.repos.d/CentOS-Base.repo
     wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
     wget -P /etc/yum.repos.d/ http://mirrors.aliyun.com/repo/epel-7.repo 
     yum clean all  
@@ -35,28 +35,32 @@ function init_repositories {
 
     # EPEL安装
     yum install -y epel-release
-    yum install -y yum-utils
     
     # nodejs 10 RPM
     curl -sL https://rpm.nodesource.com/setup_10.x | bash -
+    curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
 
     # mysql RPM
-    rpm -ivh https://dev.mysql.com/get/mysql80-community-release-el7-1.noarch.rpm
+    rpm -ivh https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
 
     # php RPM
     rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm   
     rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
     
     # nginx 
-    wget https://raw.githubusercontent.com/Chasers9527/centos-init/master/%20Centos7/config/unit.repo
+    wget https://raw.githubusercontent.com/Chasers9527/centos-init/master/Centos7/config/nginx.repo
+    mv unit.repo /etc/yum.repos.d/nginx.repo
     yum-config-manager --enable nginx-mainline
+
+    #redis
+    yum install epel-release
 
     yum -y update
 }
 
 function install_basic_softwares {
     yum install -y gcc-c++ make
-    yum install -y curl git build-essential unzip supervisor wget
+    yum install -y git build-essential unzip supervisor wget
 }
 
 function install_node_yarn {
@@ -69,20 +73,19 @@ function install_php {
     yum -y remove php*
     yum -y install php72w php72w-cli php72w-common php72w-curl php72w-pear php72w-devel php72w-embedded php72w-fpm php72w-gd php72w-mbstring \
                     php72w-mysqlnd php72w-opcache php72w-pdo php72w-xml php72w-zip php72w-pgsql php72w-sqlite3
-    pecl install swoole
-    echo "extension=swoole.so" >> /etc/php.ini
-    pecl install redis
-    echo "extension=redis.so" >> /etc/php.ini
+    # pecl install swoole
+    # echo "extension=swoole.so" >> /etc/php.ini
+    # pecl install redis
+    # echo "extension=redis.so" >> /etc/php.ini
 }
 
 function install_others {
     yum remove -y apache2
     
     # 关闭 mysql 80 使用 mysql 5.7
-    yum-config-manager --disable mysql80-community
-    yum-config-manager --enable mysql57-community
+    yum install -y mysql-community-server
 
-    yum install -y nginx mysql-community-server redis-server sqlite-devel
+    yum install -y nginx redis sqlite-devel
     chown -R ${WWW_USER}.${WWW_USER_GROUP} /var/www/
     systemctl enable nginx.service mysqld
     systemctl start mysqld
