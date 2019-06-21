1a792d73e6b4c6b4b6c451b561c6623a4fdb19e1
laragh <laragh@163.com>
2019-06-21 18:10:45 +0800

    init

--- download.sh
+++ download.sh
@@ -14,7 +14,8 @@ nc="\e[0m"
 HOME="/home"
 
 echo -e "${green}===> 开始下载...${nc}"
-cd ${HOME}
+HOME="/home"
+cd "${HOME}"
 wget -q https://github.com/Chasers9527/centos-init/archive/master.zip -O centos-init.tar.gz
 rm -rf centos-init
 tar zxf centos-init.tar.gz
